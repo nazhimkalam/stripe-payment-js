06decfb0ad04a9fd05fa8155a8abe68e2cbb1408
Nazhim <nazhimkalam@gmail.com>
2021-09-20 18:50:34 +0530

    cleaned react template

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-"# stripe-payment-js" 
-"# stripe-payment-js" 
+# Stripe payment gateway with react and node js
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+        <h2>Stripe payment checkout with react and node js</h2>
+        
+    </Container>
+  )
 }
 
-export default App;
+export default App
+
+const Container = styled.div``;
